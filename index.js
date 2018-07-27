aa1a9a14452ec21ef9b03fd2e263aaeffd41e5ba
Yuehan Duan <Yuehan.Duan@walmartlabs.com>
2018-07-27 14:59:46 -0700

    Add goals reducer and root reducer

--- index.js
+++ index.js
@@ -52,6 +52,25 @@ function todos (state = [], action) {
     }
 }
 
+function goals (state = [], action) {
+    switch(action.type) {
+        case 'ADD_GOAL': 
+            return state.concat([action.goal])
+        case 'REMOVE_GOAL':
+            return state.filter((goal) => goal.id !== action.id)
+        default: 
+        return state
+    }
+}
+
+// root reducer
+function app (state = {}, action) {
+    return {
+        todos: todos(state.todos, action),
+        goals: goals(state.goals, action)
+    }
+}
+
 // store
 function createStore (reducer) {
     // The store should have four parts 
@@ -85,3 +104,68 @@ function createStore (reducer) {
         dispatch,
     }
 }
+
+// commands to run in console
+const store = createStore(app)
+
+store.subscribe(() => {
+  console.log('The new state is: ', store.getState())
+})
+
+store.dispatch({
+  type: 'ADD_TODO',
+  todo: {
+    id: 0,
+    name: 'Walk the dog',
+    complete: false,
+  }
+})
+
+store.dispatch({
+  type: 'ADD_TODO',
+  todo: {
+    id: 1,
+    name: 'Wash the car',
+    complete: false,
+  }
+})
+
+store.dispatch({
+  type: 'ADD_TODO',
+  todo: {
+    id: 2,
+    name: 'Go to the gym',
+    complete: true,
+  }
+})
+
+store.dispatch({
+  type: 'REMOVE_TODO',
+  id: 1
+})
+
+store.dispatch({
+  type: 'TOGGLE_TODO',
+  id: 0
+})
+
+store.dispatch({
+  type: 'ADD_GOAL',
+  goal: {
+    id: 0,
+    name: 'Learn Redux'
+  }
+})
+
+store.dispatch({
+  type: 'ADD_GOAL',
+  goal: {
+    id: 1,
+    name: 'Lose 20 pounds'
+  }
+})
+
+store.dispatch({
+  type: 'REMOVE_GOAL',
+  id: 0
+})
