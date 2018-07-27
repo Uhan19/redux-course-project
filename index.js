abd5f3b173f409cf62a214228fde478a59c60ea0
Yuehan Duan <Yuehan.Duan@walmartlabs.com>
2018-07-27 10:46:48 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,81 @@
+// actions - whenever we want to change the state of app, we dispatch an action
+{
+    type: 'Add_TODO',
+    todo: {
+        id: 0,
+        name: 'Learn Redux',
+        complete: false,
+    }
+}
+
+{
+    type: 'REMOVE_TODO',
+    id: 0,
+}
+
+{
+    type: 'TOGGGLE_TODO',  
+    id: 0,
+}
+
+{
+    type: 'ADD_GOAL',
+    goal: {
+        id: 0,
+        name: 'Run a Marathon', 
+    }
+}
+
+{
+    type: 'REMOVE_GOAL',
+    id: 0,
+}
+
+// reducers
+    // has to be pure functions 
+    /* Characteristics of a Pure Function 
+        1. They always return the same result if the same arguments are passed in.
+        2. They depend only on the arguments passed into them. 
+        3. Never produce any side effects. 
+    */
+function todos (state = [], action) {
+    if (action.type === 'ADD_TODO') {
+        return state.concat([action.todo])
+    }
+
+    return state
+}
+
+// store
+function createStore (reducer) {
+    // The store should have four parts 
+    // 1. The state 
+    // 2. Get the state. 
+    // 3. Listen to changes on the state. 
+    // 4. Update the state
+
+    let state
+    let listeners = []
+
+    const getState = () => state 
+
+    const subscribe = (listener) => {
+        listeners.push(listener)
+        return () => {
+            listeners = listeners.filter((l) => l !== listener)
+        }
+    }
+
+    const dispatch = (action) => {
+        // call todos (reducer)
+        state = reducer(state, action)
+        // loop over listeners and invoke them 
+        listeners.forEach((listener) => listener())
+    }
+
+    return { 
+        getState,
+        subscribe,
+        dispatch,
+    }
+}
